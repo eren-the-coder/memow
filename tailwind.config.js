b872e42983d03fa51a5ccc925b6d2e2a8bbdbaed
Eren <jeanpc568@gmail.com>
2026-05-01 18:33:55 +0100

    fix: dependency conflict between vite@6.x and vite-plugin-pwa@0.19.8

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        'xl': '0.75rem',
+      }
+    },
+  },
+  plugins: [],
+}
